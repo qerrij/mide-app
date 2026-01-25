dcede1be66e40414e41d1abe25e1d880dde0c61f
qerrij <twt567twt@mail.ru>
2026-01-26 00:08:39 +0300

    prod

--- frontend/mide-app/src/api/axios.ts
+++ frontend/mide-app/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 // Основной instance для JSON данных
 const axiosInstance = axios.create({
